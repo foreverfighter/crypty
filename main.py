aa1af919c40df77478c89934209c3960dbee7e19
Siow Yi Sheng <siowyisheng@gmail.com>
2018-09-10 12:44:09 +0800

    Does a bit of refactoring

--- main.py
+++ main.py
@@ -3,28 +3,27 @@
 from cryptography.fernet import Fernet
 
 
-def encrypt(s):
+def encrypt(s, key=None):
     """Takes UTF-8 string and spits UTF-8 token"""
-    key = Fernet.generate_key()
-    f = Fernet(key)
-    bytes = s.encode('utf8')
-    token = f.encrypt(bytes)
-    s_key = key.decode('utf8')
-    s_token = token.decode('utf8')
+    if key is None:
+        key = Fernet.generate_key()
+    f = Fernet(to_bytes(key))
+    token = f.encrypt(to_bytes(s))
+    s_key, s_token = key.decode('utf8'), token.decode('utf8')
     return s_key, s_token
 
 
-def decrypt(key, token):
+def decrypt(token, key):
     """Takes token in UTF-8 (or bytestring) and spits UTF-8 string"""
+    f = Fernet(to_bytes(key))
+    bytes = f.decrypt(to_bytes(token))
+    return bytes.decode('utf8')
+
+
+def to_bytes(str_bytes):
+    """Takes UTF-8 string or bytes and safely spits out bytes"""
     try:
-        f = Fernet(key)
-    except TypeError:
-        key = key.encode('utf8')
-        f = Fernet(key)
-    try:
-        bytes = f.decrypt(token)
-    except TypeError:
-        token = token.encode('utf8')
-        bytes = f.decrypt(token)
-    s = bytes.decode('utf8')
-    return s
+        bytes = str_bytes.encode('utf8')
+    except AttributeError:
+        return str_bytes
+    return bytes
